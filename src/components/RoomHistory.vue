94886773f272c0fa096704a51407f3fdf473d705
Lukas Baginski <lukas.baginski@ambilstein.de>
2020-11-19 18:56:59 +0100

    Changed RoomHistory but still not working.

--- src/components/RoomHistory.vue
+++ src/components/RoomHistory.vue
@@ -1,5 +1,7 @@
 <template>
     <v-container>
+        <div class="container vertical flat" id="bar_chart">
+        </div>
     </v-container>
 </template>
 
@@ -8,38 +10,44 @@ export default {
     name: 'RoomHistory',
 
     data: () => ({
+        bar_chart_element: '<div class="progress-bar"><div class="progress-track"><div class="progress-fill"><span>{0}</span></div></div></div>',
+        values: [536, 241, 799, 901, 432, 756, 100, 0, 2, 999], //get data from server
+        times: [],
+        width: '' + (this.values.length * 45 + 24),
+        position: 0,
+        maxValue: 1000,
     }),
 
     mounted() {
-        var bar_chart_element = '<div class="progress-bar"><div class="progress-track"><div class="progress-fill"><span>%s</span></div></div></div>';
-        var values = [536, 241, 799, 901, 432, 756, 100, 0, 2, 999]; //get data from server
-        var times = [];
-        var maxValue = 1000;
-        var width = '' + (values.length * 45 + 24);
-        var position = 0;
+        //var bar_chart_element = '<div class="progress-bar"><div class="progress-track"><div class="progress-fill"><span>%s</span></div></div></div>';
+        //var values = [536, 241, 799, 901, 432, 756, 100, 0, 2, 999]; //get data from server
+        //var times = [];
+        //var maxValue = 1000;
+        //var width = '' + (values.length * 45 + 24);
+        //var position = 0;
 
         //Error: '$' is not defined. How to use this in Vue?
-        $('#bar_chart').css({
-            'width': width,
+        document.getElementById('bar_chart').css({
+            'width': this.width,
             'height': '320px'
         })
 
-        values.forEach(function(value) {
-            if (value == 'why is an unused variable an error in js?') {values[0] = value;}
-            $('#bar_chart').append(bar_chart_element.replace("%s", [times[position]])); //not working
-            position += 1;
+        this.values.forEach(function(value) {
+            if (value == 'why is an unused variable an error in js?') {this.values[0] = value;}
+            document.getElementById('bar_chart').append(this.bar_chart_element.replace("%s", [this.times[this.position]])); //not working
+            this.position += 1;
         });
 
-        position = 0;
-        $('.vertical .progress-fill span').each(function() {
-            var percent = (values[position] / maxValue * 100) + '%';
+        this.position = 0;
+        document.getElementsByClassName('.vertical .progress-fill span').each(function(elem) {
+            var percent = (this.values[this.position] / this.maxValue * 100) + '%';
             var pTop = 100 - ( percent.slice(0, percent.length - 1) ) + "%"
-            $(this).parent().css({
+            elem.parent().css({
                 'height' : percent,
                 'top' : pTop,
                 'background-color' : 'rgb(' + this.farbBerechnung(50) + ')'
             });
-        position += 1;
+            this.position += 1;
       });
     },
 
