c29a6821239cc08137169b80f5d9c9b5cdebd486
Lukas Baginski <lukas.baginski@ambilstein.de>
2020-11-24 08:53:27 +0100

    History may work, website didnt reload locally.

--- src/components/RoomHistory.vue
+++ src/components/RoomHistory.vue
@@ -19,20 +19,26 @@ export default {
     }),
 
     mounted() {
-        //var bar_chart_element = '<div class="progress-bar"><div class="progress-track"><div class="progress-fill"><span>%s</span></div></div></div>';
-        //var values = [536, 241, 799, 901, 432, 756, 100, 0, 2, 999]; //get data from server
-        //var times = [];
-        //var maxValue = 1000;
-        //var width = '' + (values.length * 45 + 24);
-        //var position = 0;
+        /*var bar_chart_element = '<div class="progress-bar"><div class="progress-track"><div class="progress-fill"><span>%s</span></div></div></div>';
+        var values = [536, 241, 799, 901, 432, 756, 100, 0, 2, 999]; //get data from server
+        var times = [];
+        var maxValue = 1000;
+        var width = '' + (this.values.length * 45 + 24);
+        var position = 0;*/
 
-        //Error: '$' is not defined. How to use this in Vue?
+        //Die Historie konnte nicht beendet werden, da dieser Bereich (mounted() {}) nie ausgeführt wurde.
+        //Prinzipiell sollte das Programm laufen (Code wurde aus History.html kopiert)
+
         document.getElementById('bar_chart').css({
             'width': this.width,
             'height': '320px'
         })
 
+        console.log('Ich war hier');
+        console.log(this.values);
+
         this.values.forEach(function(value) {
+            console.log("Value:" + value);
             if (value == 'why is an unused variable an error in js?') {this.values[0] = value;}
             document.getElementById('bar_chart').append(this.bar_chart_element.replace("%s", [this.times[this.position]])); //not working
             this.position += 1;
