aacb4271a86d2e7cade410846fe517473ae1a67d
KLukas04 <krinkelukas@gmail.com>
2020-12-29 12:14:40 +0100

    Backend

--- Backend.py
+++ Backend.py
@@ -44,17 +44,6 @@ def getStatusNow(raumNr):
     for (id, raum, temp, co2, h2o, score, datumuhrzeit) in cursor:
         ergebnis.append({"Raum": raum, "ID": id, "Temp": temp, "co2": co2, "h2o": h2o, "score": score, "DatumZeit": datumuhrzeit})
 
-    return jsonify(ergebnis) 
-
-@app.route("/postNow/<raumNr>/<temp>", methods=["GET"])
-def postNow(raumNr, temp):
-    db = getDB()
-    cursor = db.cursor()
-    jetzt = datetime.now().strftime("%Y-%m-%d %H:%M") 
-    cursor.execute("INSERT INTO Messwerte(raum, temp, DatumUhrzeit) VALUES(?,?,?);", (raumNr, temp, jetzt))
-    db.commit()
-    return 'success'
-
     return jsonify(ergebnis)
 
 @app.route("/postNow/<raumNr>/<temp>", methods=["GET"])
